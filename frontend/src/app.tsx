602be0d529d627b10cdd8e8f15be00ea38717851
alexogeny <alexogeny@pm.me>
2021-03-21 12:41:14 +1000

    Get react to render an index page

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,13 +1,23 @@
-import * as React from 'react';
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import "./styles.css";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql"
+});
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <h1>Citizens of the Name Change Society</h1>
+        <ApolloProvider client={client}>
+            <div className="App">
+                <h1>Citizens of the Name Change Society</h1>
 
-        </div>
+            </div>
+        </ApolloProvider>
+
     );
+
 };
 
 export default App;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Index = () => (
+    <h1>Name change society</h1>
+)
+export default Index;
